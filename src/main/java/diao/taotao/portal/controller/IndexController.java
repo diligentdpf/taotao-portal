7871bdfb4164bd863e1427aae3addb03073b6f82
diligentdpf <1852300415@qq.com>
2016-05-29 21:32:09 +0800

    实现了在门户首页动态加载大广告的功能

--- src/main/java/diao/taotao/portal/controller/IndexController.java
+++ src/main/java/diao/taotao/portal/controller/IndexController.java
@@ -1,16 +1,24 @@
 package diao.taotao.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import diao.taotao.portal.service.ContentService;
 
 @Controller
 public class IndexController {
-    /**
-     * 打开首页
-     */
-    @RequestMapping("/index")
-    public String index() {
-        return "index";
-    }
-
+	
+	@Autowired
+	ContentService contentService;
+	/**
+	 * 打开首页
+	 */
+	@RequestMapping("/index")
+	public String index(Model model) {
+		String adJson = contentService.getContentList();
+		model.addAttribute("ad1", adJson);
+		return "index";
+	}
 }
---
+++ src/main/java/diao/taotao/portal/service/ContentService.java
@@ -0,0 +1,5 @@
+package diao.taotao.portal.service;
+
+public interface ContentService {
+	public String getContentList();
+}
