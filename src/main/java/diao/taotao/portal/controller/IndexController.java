df7aa5012455acdec1fd9c776c0f578fccaed846
diligentdpf <1852300415@qq.com>
2016-05-30 18:50:35 +0800

    在indexController里面加一个httpClient/post映射用于测试

--- src/main/java/diao/taotao/portal/controller/IndexController.java
+++ src/main/java/diao/taotao/portal/controller/IndexController.java
@@ -1,24 +1,38 @@
 package diao.taotao.portal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import diao.taotao.portal.service.ContentService;
 
 @Controller
 public class IndexController {
-	
-	@Autowired
-	ContentService contentService;
-	/**
-	 * 打开首页
-	 */
-	@RequestMapping("/index")
-	public String index(Model model) {
-		String adJson = contentService.getContentList();
-		model.addAttribute("ad1", adJson);
-		return "index";
-	}
+
+    @Autowired
+    ContentService contentService;
+
+    /**
+     * 打开首页
+     */
+    @RequestMapping("/index")
+    public String index(Model model) {
+        String adJson = contentService.getContentList();
+        model.addAttribute("ad1", adJson);
+        return "index";
+    }
+
+    @RequestMapping(value = "/httpclient/post", method = RequestMethod.POST, 
+            produces = MediaType.APPLICATION_JSON_VALUE
+            + ";charset=utf-8")
+    @ResponseBody
+    public String httpClientTest(String username, String passwd) {
+        System.err.println("username:" + username + ",passwd:" + passwd);
+        // return TaotaoResult.ok();
+        return "{username:" + username + ",passwd:" + passwd + "}";
+    }
 }
