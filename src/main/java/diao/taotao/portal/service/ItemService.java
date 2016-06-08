b84b8efa89604fae7b89b5b867c6826dc6725a64
diligentdpf <1852300415@qq.com>
2016-06-08 17:09:00 +0800

    商品展示

--- src/main/java/diao/taotao/portal/service/ItemService.java
+++ src/main/java/diao/taotao/portal/service/ItemService.java
@@ -3,5 +3,18 @@ package diao.taotao.portal.service;
 import diao.taotao.portal.pojo.ItemInfo;
 
 public interface ItemService {
-    public ItemInfo getItemById(Long itemId) ;
+    /**
+     * 调用rest的服务查询商品基本信息
+     */
+    public ItemInfo getItemById(Long itemId);
+
+    /**
+     * 调用rest的服务查询商品描述信息
+     */
+    public String getItemDescById(Long itemId);
+
+    /**
+     * 调用rest的服务查询商品规格信息
+     */
+    public String getItemParam(Long itemId);
 }
